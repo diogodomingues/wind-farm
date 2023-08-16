576e1e7c34cd06de7ebd198d7fb6b290ffc99f86
Diogo <diogo.crack@gmail.com>
2023-08-16 23:36:49 +0100

    update default blades and register blades and dashboard blades for layout

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,25 +13,45 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('Wind Farm') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('turbine.get')" :active="request()->routeIs('turbine.get')">
+                        {{ __('Turbines') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('component.get')" :active="request()->routeIs('component.get')">
+                        {{ __('Components') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('inspection.get')" :active="request()->routeIs('inspection.get')">
+                        {{ __('Inspections') }}
                     </x-nav-link>
                 </div>
             </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
+                <div class="row">
+                    <div class="col">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
-
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </div>
                         </button>
+                    </div>
+                    <div class="col">
+                        <form action="{{route('logout')}}" method="POST">
+                            @csrf
+                            <input class="btn btn-sm btn-outline-dark" type="submit" value="Logout" />
+                        </form>
+                    </div>
+                </div>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+
                     </x-slot>
+
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
@@ -41,10 +61,7 @@
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
+                            <x-dropdown-link :href="route('logout')">
                                 {{ __('Log Out') }}
                             </x-dropdown-link>
                         </form>
@@ -88,8 +105,7 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
+                    <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
                     </x-responsive-nav-link>
